bbee43339bb8b733ae3f6db1d8725efe067c1d8c
kritikhatter <83900188+kritikhatter@users.noreply.github.com>
2021-06-11 08:27:03 +0530

    Add files via upload

---
+++ testingScript.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyDTbi36WeW5-QcTDG3JzKpspDW7Oey3dFY",
+  authDomain: "aca-25.firebaseapp.com",
+  databaseURL: "https://aca-25-default-rtdb.firebaseio.com",
+  projectId: "aca-25",
+  storageBucket: "aca-25.appspot.com",
+  messagingSenderId: "951338423127",
+  appId: "1:951338423127:web:0a1330120f8d4a8d5de412"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+  
+  
